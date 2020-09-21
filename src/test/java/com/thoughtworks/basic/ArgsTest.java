9af6aafd1bb4b173852116bcc3918c778e480c45
fws06118 <1241786550@qq.com>
2020-09-22 00:33:52 +0800

    [fws]should_return_string_list_when_arg_given_no_value_finish

--- src/test/java/com/thoughtworks/basic/ArgsTest.java
+++ src/test/java/com/thoughtworks/basic/ArgsTest.java
@@ -10,14 +10,14 @@ import static org.junit.Assert.assertThat;
 
 public class ArgsTest {
     @Test
-    public void should_return_string_list_when_scan_givrn_string() throws Exception {
+    public void should_return_string_list_when_arg_given_no_value() throws Exception {
         String inputArgs = "-l  -p  -d ";
 
         Args args = new Args(inputArgs);
 
-        assertEquals("[FlagSchema{flag='l  ', value=false, type='boolean'}, " +
-                        "FlagSchema{flag='p  ', value=0, type='int'}, " +
-                        "FlagSchema{flag='d ', value=, type='class java.lang.String'}]"
+        assertEquals("[FlagSchema{flag='l', value=false, type='boolean'}, " +
+                        "FlagSchema{flag='p', value=0, type='int'}, " +
+                        "FlagSchema{flag='d', value=, type='class java.lang.String'}]"
                 ,args.getFlagSchemaList().toString());
 
     }
