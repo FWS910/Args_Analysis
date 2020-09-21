07d4d63c9b7eccffe60b36bc7a43c7ed14f41755
fws06118 <1241786550@qq.com>
2020-09-22 00:28:59 +0800

    [fws]should_return_string_list_when_arg_given_no_value

---
+++ src/test/java/com/thoughtworks/basic/ArgsTest.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.basic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class ArgsTest {
+    @Test
+    public void should_return_string_list_when_scan_givrn_string() throws Exception {
+        String inputArgs = "-l  -p  -d ";
+
+        Args args = new Args(inputArgs);
+
+        assertEquals("[FlagSchema{flag='l  ', value=false, type='boolean'}, " +
+                        "FlagSchema{flag='p  ', value=0, type='int'}, " +
+                        "FlagSchema{flag='d ', value=, type='class java.lang.String'}]"
+                ,args.getFlagSchemaList().toString());
+
+    }
+
+
+}
